a4e91c04b0579f5eb3f93bd35fc5352feec202bb
Krystian <krystian.kuzminski@programa.pl>
2024-01-22 13:15:59 +0100

    Add clear function

--- main.js
+++ main.js
@@ -3,8 +3,14 @@
   const buttons = document.querySelectorAll('.calculator-buttons input[type="button"]')
   const resultButton = document.querySelector('#result-button');
   const result = document.querySelector('.calculator-results');
+  const clearButton = document.querySelector('#clear-button')
   const display = (value) => {
     headerDisplay.value += value;
+  }
+
+  const clear = () => {
+    headerDisplay.value = '';
+    result.value = '';
   }
 
   buttons.forEach(button => {
@@ -16,5 +22,9 @@
 
   resultButton.addEventListener('click', () => {
     result.value = '0';
-  });
+  }); 
+
+  clearButton.addEventListener('click', () => {
+    clear();
+  })
 })()
