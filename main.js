1a34e1812adb4feb187ea4c1451388559e3245d1
Krystian <krystian.kuzminski@programa.pl>
2024-01-22 12:45:16 +0100

    Display user input

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 (() => {
-  console.log('hi there!')
-});
+  const headerDisplay = document.querySelector('#display');
+  const buttons = document.querySelectorAll('.calculator-buttons input[type="button"]')
+
+  const display = (value) => {
+    headerDisplay.value += value;
+  }
+
+  buttons.forEach(button => {
+    button.addEventListener('click', (el) => {
+      const value = el.target.value;
+      display(value);
+    })
+  })
+})()
